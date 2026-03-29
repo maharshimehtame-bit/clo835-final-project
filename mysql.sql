c049441e2d0086995f2f7ebc080f0319adc8591e
Ghufran Ataie <ghufranataie@hotmail.com>
2026-03-29 10:52:33 -0400

    Configuration Files Updated By Ghufran

--- mysql.sql
+++ mysql.sql
@@ -8,7 +8,11 @@ last_name VARCHAR(20),
 primary_skill VARCHAR(20),
 location VARCHAR(20));
 
-INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee 
+(emp_id, first_name, last_name, primary_skill, location) VALUES 
+('1','Amanda','Williams','Smile','local'),
+('2','Maharshi','Mehta','System Engineer','North York'),
+('3','Ghufran','Ataie','DB Developer','Ajax'),
+('4','Vibha','Thakkar','Marketing Officer','Scarborough')
+;
 SELECT * FROM employee;
-
